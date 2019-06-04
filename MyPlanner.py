a5e215e1eb19a331764dbcd393ff300bec65fe7a
Kathleen Silva <kathleensn@al.insper.edu.br>
2019-06-03 22:11:25 -0300

    Organizando o código

--- MyPlanner.py
+++ MyPlanner.py
@@ -1,5 +1,4 @@
-﻿import sqlite3
-from tkinter import *
+﻿from tkinter import *
 import tkinter as tk
 import json
 
@@ -21,12 +20,12 @@ class TelaPrincipal(tk.Frame):
         tk.Frame.__init__(self, app.root)
         
         self.app = app
+        
 #--------------------------------------TKINTER INTERFACE/ TELA DE CADASTRO ------------------------------------------------#
         self.app.root.geometry("775x600")
          
-         #Configurações do design
-         #linhas
-        self.rowconfigure(0, minsize=100, weight=1) 
+        #Configurações do design
+        self.rowconfigure(0, minsize=100, weight=1)#linhas 
         self.rowconfigure(1, minsize=25, weight=1)
         self.rowconfigure(2, minsize=25, weight=1)
         self.rowconfigure(3, minsize=25, weight=1)
@@ -42,8 +41,7 @@ class TelaPrincipal(tk.Frame):
         self.rowconfigure(13, minsize=50, weight=1)
         self.rowconfigure(14, minsize=150, weight=1)
       
-         # colunas
-        self.columnconfigure(0, minsize = 100, weight=1) 
+        self.columnconfigure(0, minsize = 100, weight=1) # colunas
         self.columnconfigure(1, minsize = 50, weight=1)
         self.columnconfigure(2, minsize = 50, weight=1)
         self.columnconfigure(3, minsize = 275, weight=1)
@@ -75,7 +73,7 @@ class TelaPrincipal(tk.Frame):
         self.nome.grid(row=2, column=2, sticky="nsew")
         self.nome["bg"] = "light sky blue"
        
-         # Informacoes de idade do usuario:
+        # Informacoes de idade do usuario:
         self.entrada2 = tk.Entry(self)
         self.entrada2.configure(textvariable = self.caixa_texto2)
         self.entrada2.grid(row=5, column=3, sticky="nsew")   
@@ -115,8 +113,9 @@ class TelaPrincipal(tk.Frame):
             dicionario[self.app.nome] = {"idade": self.app.idade, "ocupacao": self.app.ocup, "tarefas a fazer": []}
             # abre 'cadastros realizados.txt' sem apagar o que já existia 
             with open("cadastros realizados.json", "a") as arquivo:
-                arquivo.write(json.dumps(dicionario)) 
-    #defini tela de cadastro e puxa os dados digitados       
+                arquivo.write(json.dumps(dicionario))
+                
+    # defini tela de cadastro e puxa os dados digitados       
     def cadastrausuario(self):
         self.app.nome = self.entrada1.get()
         self.app.idade = self.entrada2.get()
@@ -132,69 +131,51 @@ class CadastroFeito(tk.Frame):
         
         self.app = app
 
-        #Configurações do design
-        self.rowconfigure(0, minsize=100, weight=1) #linhas
+        # Configurações do design
+        self.rowconfigure(0, minsize=100, weight=1)# linhas
         self.rowconfigure(1, minsize=100, weight=1)
         self.rowconfigure(2, minsize=100, weight=1)
         self.rowconfigure(3, minsize=100, weight=1)
         self.rowconfigure(4, minsize=100, weight=1)
         self.rowconfigure(5, minsize=100, weight=1)
         
-        self.columnconfigure(0, minsize=375, weight=1) #colunas
+        self.columnconfigure(0, minsize=375, weight=1)# colunas
         self.columnconfigure(1, minsize=200, weight=1)
         self.columnconfigure(2, minsize=50, weight=1)
         self.columnconfigure(3, minsize=50, weight=1)
 
+        self.configure(background="light sky blue")# definindo cor do fundo
         
-
-        self.configure(background="light sky blue") #definindo cor do fundo
-    
-        self.titulo1 = tk.Label(self, text="Minhas tarefas") 
+        # titulo da tela
+        self.titulo1 = tk.Label(self, text="Minhas Tarefas") 
         self.titulo1["font"] = ("Times New Roman", "30", "bold")
         self.titulo1.grid(row=1, column=0, sticky="n")
         self.titulo1["foreground"]='black'
         self.titulo1["bg"]='light sky blue'
         
-        #criando listbox pra armazenar as tarefas digitadas e aderindo o delete // posicionando-o 
+        # criando listbox pra armazenar as tarefas digitadas e aderindo o delete // posicionando-o 
         self.tarefas = tk.Listbox(self)
         self.tarefas.grid(row=2, column=0, rowspan=3, columnspan=2, 
                           sticky="nsew", padx=5)
         self.tarefas.bind("<Delete>", self.app.apertou_delete)
         
-        #criando jeito de usar o conteudo digitado
+        # criando jeito de usar o conteudo digitado
         self.conteudo_caixa_texto = tk.StringVar()
         
-        #criando caixa de texto, a posicionando e aderindo o botão de enter nela
+        # criando caixa de texto, a posicionando e aderindo o botão de enter nela
         self.caixa_texto = tk.Entry(self)
         self.caixa_texto.configure(textvariable=self.conteudo_caixa_texto)
         self.caixa_texto.grid(row=5, column=0, columnspan=2, 
                               sticky="new", padx=5, pady=5)  
         self.caixa_texto.bind("<Return>", self.app.apertou_enter)
         
-        #configurações do botão salvar que salva as tarefas no listbox
-        self.salvar = tk.Button(self)
-        self.salvar["text"] = "Salvar"
-        self.salvar["font"] = ("Times New Roman", "15", "bold") 
-        self.salvar["command"] = self.app.salvar
-        self.salvar.grid(row=3, column=2, sticky="")
-        self.salvar["bg"] = "tomato3"
-        
-        #configurações do botão apagar
-        self.apagar = tk.Button(self)
-        self.apagar["text"] = "Apagar"
-        self.apagar["font"] = ("Times New Roman", "15", "bold") 
-        self.apagar["command"] = self.app.apagar
-        self.apagar.grid(row=5, column=2, sticky="")
-        self.apagar["bg"] = "tomato3"
-        
-        #configurações do label que mostra as infos do perfil
+        # configurações do label que mostra as infos do perfil
         self.perfil = tk.Label(self, text="Perfil \n Nome: {0} \n Idade: {1} \n Ocupação: {2}".format(self.app.nome, self.app.idade, self.app.ocup))
         self.perfil["font"] = ("Times New Roman", "15") 
         self.perfil.grid(row=0, column=2, sticky="")
         self.perfil["bg"] = "tomato3"
                 
-         
-        #configurações do botão que leva a tela de tarefas realizadas
+        # configurações do botão que leva a tela de tarefas realizadas
         self.tarefas_realizadas = tk.Button(self) 
         self.tarefas_realizadas["text"] = "Tarefas Realizadas"
         self.tarefas_realizadas["font"] = ("Times New Roman","15", "bold")
@@ -202,7 +183,23 @@ class CadastroFeito(tk.Frame):
         self.tarefas_realizadas.grid(row=1, column=2, sticky="")
         self.tarefas_realizadas["bg"] = "tomato3"
         
-        #configurações do botão feita que manda as tarefas para tela de realizadas
+        # configurações do botão salvar que salva as tarefas no listbox
+        self.salvar = tk.Button(self)
+        self.salvar["text"] = "Salvar"
+        self.salvar["font"] = ("Times New Roman", "15", "bold") 
+        self.salvar["command"] = self.app.salvar
+        self.salvar.grid(row=3, column=2, sticky="")
+        self.salvar["bg"] = "tomato3"
+        
+        # configurações do botão apagar
+        self.apagar = tk.Button(self)
+        self.apagar["text"] = "Apagar"
+        self.apagar["font"] = ("Times New Roman", "15", "bold") 
+        self.apagar["command"] = self.app.apagar
+        self.apagar.grid(row=5, column=2, sticky="")
+        self.apagar["bg"] = "tomato3"
+        
+        # configurações do botão feita que manda as tarefas para tela de realizadas
         self.feito = tk.Button(self) 
         self.feito["text"] = "Feita!"
         self.feito["font"] = ("Times New Roman", "15", "bold")
@@ -213,34 +210,40 @@ class CadastroFeito(tk.Frame):
     def update(self):
         self.perfil["text"] = "Perfil \n Nome: {0} \n Idade: {1} \n Ocupação: {2}".format(self.app.nome, self.app.idade, self.app.ocup)
 
+
 class TarefasRealizadas(tk.Frame):
     def __init__(self, app):
         tk.Frame.__init__(self, app.root)
         
         self.app = app
-        #------------------design da tela
-        self.rowconfigure(0, minsize=100, weight=1) # linhas
+        
+        # design da tela
+        self.rowconfigure(0, minsize=100, weight=1)# linhas
         self.rowconfigure(1, minsize=300, weight=1)
         self.rowconfigure(2, minsize=100, weight=1)
         self.rowconfigure(3, minsize= 100, weight=1)
         
-        self.columnconfigure(0, minsize=200, weight=1) #colunas
+        self.columnconfigure(0, minsize=200, weight=1)# colunas
         self.columnconfigure(1, minsize=300, weight=1)
         self.columnconfigure(2, minsize=200, weight=1)
         self.columnconfigure(3, minsize = 75, weight=1)
         
-        self.configure(background="light sky blue")
+        self.configure(background="light sky blue")# definindo cor da tela
         
-        #--------------------
         
-        #configurações do label do título
+        # configurações do listbox de tarefas feitas
+        self.lista_feito = tk.Listbox(self) #cria listbox pras tarefas feitas
+        self.lista_feito.grid(row=1, column=0, rowspan=2, columnspan=4, sticky="nsew", padx=5, pady=5) #posiciona na tela
+        self.lista_feito.bind("<Delete>", self.app.apertou_delete) #aciona o botão delete
+        
+        # configurações do label do título
         self.titulo = tk.Label(self, text="Tarefas Realizadas")
         self.titulo["font"] = ("Times New Roman", "20", "bold")
         self.titulo.grid(row=0, column=0, columnspan=3, sticky="", padx=5, pady=5)
         self.titulo["foreground"]='black'
         self.titulo["bg"]='light sky blue'
                
-        #configurações do botão voltar 
+        # configurações do botão voltar 
         self.voltar = tk.Button(self) #botão 1
         self.voltar["text"] = "Voltar"
         self.voltar["font"] = ("Times New Roman", "15", "bold")
@@ -248,7 +251,7 @@ class TarefasRealizadas(tk.Frame):
         self.voltar.grid(row=3, column=0, sticky="sw", padx=1, pady=1)
         self.voltar["bg"] = "tomato3"
 
-        #configurações do botão apagar
+        # configurações do botão apagar
         self.apagar = tk.Button(self) 
         self.apagar["text"] = "Apagar"
         self.apagar["font"] = ("Times New Roman", "15", "bold") 
@@ -256,11 +259,7 @@ class TarefasRealizadas(tk.Frame):
         self.apagar.grid(row=3, column=3, sticky="se", padx=1, pady=1)
         self.apagar["bg"] = "tomato3"
         
-        #configurações do listbox de tarefas feitas
-        self.lista_feito = tk.Listbox(self) #cria listbox pras tarefas feitas
-        self.lista_feito.grid(row=1, column=0, rowspan=2, columnspan=4, sticky="nsew", padx=5, pady=5) #posiciona na tela
-        self.lista_feito.bind("<Delete>", self.app.apertou_delete) #aciona o botão delete
-
+        
 class Aplicação:
     def __init__(self):
         self.nome = ""
@@ -268,21 +267,29 @@ class Aplicação:
         self.idade = ""
         
         self.root = tk.Tk()
-        self.root.geometry("775x600")
-        self.root.title(string='MyPlanner')
+        self.root.geometry("775x600") # tamanho da tela
+        self.root.title(string='MyPlanner') # titulo da aplicação
         
-        self.tela_principal = TelaPrincipal(self)  # Tela principal.
-        self.botaocadastra = CadastroFeito(self)
-        self.tarefas_realizadas = TarefasRealizadas(self) 
+        self.tela_principal = TelaPrincipal(self )# Tela principal/cadastro
+        self.botaocadastra = CadastroFeito(self) # Tela após a de cadastro/ de tarefas
+        self.tarefas_realizadas = TarefasRealizadas(self) # Tela de tarefas realizadas 
         
-        self.tela_atual = self.tela_principal
+        self.tela_atual = self.tela_principal #definindo tela
         self.tela_atual.grid()
         
+    def cadastrausuario(self):
+        self.tela_atual.grid_forget()
+        self.botaocadastra.update()
+        self.botaocadastra.grid()
+        self.tela_atual = self.botaocadastra      
+    
+    # função para mudar da tela de cadastro para a tela de tarefas
     def mudar_tela_principal(self):
         self.tela_atual.grid_forget()
         self.botaocadastra.grid()
         self.tela_atual = self.botaocadastra 
-
+        
+    # função para salvar a tarefa 
     def salvar(self):
         if len(self.tela_atual.conteudo_caixa_texto.get()) == 0:
             print("digite uma tarefa")
@@ -292,12 +299,13 @@ class Aplicação:
                             self.tela_atual.conteudo_caixa_texto.get())
             
             self.tela_atual.conteudo_caixa_texto.set("")
-            self.salva_tarefas(salvando_tarefas) 
-        
-    def apertou_delete(self, event):
-        self.apagar()
-        self.apagar1()
-        
+            self.salva_tarefas(salvando_tarefas)
+            
+    # função que faz com que o botão enter funcione                
+    def apertou_enter(self, event):
+        self.salvar()
+            
+    # função para apagar uma tarefa
     def apagar(self):
         items = self.botaocadastra.tarefas.curselection()
         pos = 0
@@ -305,7 +313,8 @@ class Aplicação:
             idx = int(i) - pos
             self.botaocadastra.tarefas.delete(idx, idx)
             pos = pos + 1
-            
+    
+    # função para apagar uma tarefa da tela de tarefas realizdas         
     def apagar1(self):
         items = self.tarefas_realizadas.lista_feito.curselection()
         pos = 0
@@ -313,16 +322,12 @@ class Aplicação:
             idx = int(i) - pos
             self.tarefas_realizadas.lista_feito.delete( idx,idx )
             pos = pos + 1
+     
+    # função que faz com que o botão de delete funcione
+    def apertou_delete(self, event):
+        self.apagar()
+        self.apagar1()
     
-    def cadastrausuario(self):
-        self.tela_atual.grid_forget()
-        self.botaocadastra.update()
-        self.botaocadastra.grid()
-        self.tela_atual = self.botaocadastra  
-                    
-    def apertou_enter(self, event):
-        self.salvar()
-        
     def tarefas_feitas(self):
         self.tela_atual.grid_forget()
         self.tarefas_realizadas.grid()
@@ -348,7 +353,7 @@ class Aplicação:
         with open("cadastros realizados.json", "a") as arquivo:
             arquivo.write(json.dumps(dicionario)) 
            
-
+    #função que faz com que o código rode
     def roda(self):
         self.root.mainloop()
 
